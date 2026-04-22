33f9995f62c2b6f92b776c2cd02b75b020e37f61
Josh Cook <joshuacook0023@gmail.com>
2026-04-22 08:03:56 -0700

    chore: clean up agents code and some Ollama helpers

--- src/clurb/agents.py
+++ src/clurb/agents.py
@@ -1,10 +1,12 @@
 """Research agent."""
 
 from datetime import datetime
-from typing import Any
+from typing import Any, Literal
 
-from deepagents import create_deep_agent
+import ollama
+from deepagents import SubAgent, create_deep_agent
 from langchain.chat_models import init_chat_model
+from loguru import logger
 
 from clurb.prompts import (
     RESEARCH_WORKFLOW_INSTRUCTIONS,
@@ -14,11 +16,42 @@ from clurb.prompts import (
 from clurb.tools import tavily_search, think_tool
 
 
-def build_research_agent() -> Any:
+def list_ollama_models() -> list[str]:
+    """List available Ollama models."""
+    return [m.model for m in ollama.list().models if m.model]
+
+
+def check_ollama_model(model: str) -> bool:
+    """Check an Ollama model."""
+    res = ollama.generate(
+        model=model,
+        prompt=(
+            "Confirm you are operational by responding 'true'. "
+            "Otherwise, respond 'false'."
+        ),
+        stream=False,
+    )
+    if not res.done:
+        logger.warning("Incomplete response returned by model.")
+        return False
+
+    response = res.response.strip()
+    if response == "true":
+        return True
+
+    if response == "false":
+        logger.warning("Model says it is not ready.")
+        return False
+
+    logger.warning(f"Model did not respond as prompted. Models response: {res}.")
+    return False
+
+
+def build_research_agent(model: str, provider: Literal["ollama"] = "ollama") -> Any:
     """Build a research AI agent."""
     # Limits
-    max_concurrent_research_units = 3
-    max_researcher_iterations = 3
+    max_concurrent_research_units = 1
+    max_researcher_iterations = 1
 
     # Get current date
     current_date = datetime.now().strftime("%Y-%m-%d")
@@ -36,27 +69,23 @@ def build_research_agent() -> Any:
     )
 
     # Create research sub-agent
-    research_sub_agent = {
-        "name": "research-agent",
-        "description": "Delegate research to the sub-agent researcher. Only give "
-        "this researcher one topic at a time.",
-        "system_prompt": RESEARCHER_INSTRUCTIONS.format(date=current_date),
-        "tools": [tavily_search, think_tool],
-    }
-
-    # Model Gemini 3
-    # model = ChatGoogleGenerativeAI(model="gemini-3-pro-preview", temperature=0.0)
-
-    # Model Claude 4.5
-    model = init_chat_model(
-        model="anthropic:claude-sonnet-4-5-20250929",
-        temperature=0.0,
+    research_sub_agent = SubAgent(
+        name="research-agent",
+        description=(
+            "Delegate research to the sub-agent researcher. Only give "
+            "this researcher one topic at a time."
+        ),
+        system_prompt=RESEARCHER_INSTRUCTIONS.format(date=current_date),
+        tools=[tavily_search, think_tool],
     )
+
+    # Create the base chat model.
+    chat_model = init_chat_model(model=f"{provider}:{model}")
 
     # Create the agent
     return create_deep_agent(
-        model=model,
+        model=chat_model,
         tools=[tavily_search, think_tool],
         system_prompt=instructions,
-        subagents=[research_sub_agent],  # type: ignore
+        subagents=[research_sub_agent],
     )
